bd61959c4e24319f469a616a0db2db3e3bf309ca
Eniidras <eniidras@yahoo.com>
2023-01-13 01:33:53 +0100

    api

--- main.py
+++ main.py
@@ -15,7 +15,37 @@ app.secret_key = "T0to_na_p4s_d0rm1"
 
 @app.route("/", methods = ['GET'])
 def home():
+    imgs = os.listdir("static/images/")
+    for img in imgs:
+        flash(img, "img")
     return render_template('home.html')
+
+def no_body(message):
+    message = {
+        "message" : message
+    }
+    return jsonify(message), 400
+
+@app.route("/api", methods=["POST"])
+def api():
+    if not request.is_json:
+        return no_body("Pas de document json dans le corps de la requête.")
+
+    json_data = request.json
+    if "img_name" not in json_data.keys():
+        return no_body("Le corps de la requête n'a pas de champs 'img_name'.")
+    img_name = json_data["img_name"]
+
+    try:
+        pmask = modele.treat_image(img_name)
+    except cv2.error:
+        return no_body("L'image demandée n'existe pas.")
+    res = Image.fromarray(pmask.astype(np.uint8))
+
+    img_data = io.BytesIO()
+    res.save(img_data, "PNG")
+    encoded_res_data = base64.b64encode(img_data.getvalue()).decode('utf-8')
+    return jsonify({"mask" : encoded_res_data})
 
 @app.route("/application_web", methods=["GET"])
 def application():
@@ -23,18 +53,6 @@ def application():
     for img in imgs:
         flash(img, "img")
     return render_template('application_web.html')
-
-@app.route("/api", methods=["POST"])
-def api():
-    # change that : img_name = request.args.get("img_name")
-    img_name = None
-    pmask = modele.treat_image(img_name)
-    res = Image.fromarray(pmask.astype(np.uint8))
-
-    data = io.BytesIO()
-    res.save(data, "PNG")
-    encoded_res_data = base64.b64encode(data.getvalue())
-    return # autre chose render_template("rendu.html", img_data=encoded_img_data.decode('utf-8'))
 
 @app.route("/application_web/rendu", methods=["GET"])
 def render():
@@ -82,19 +100,6 @@ def render():
         pmask_data=encoded_pmask_data.decode('utf-8'),
         img_with_tmask_data=encoded_img_with_tmask_data.decode('utf-8'),
         img_with_pmask_data=encoded_img_with_pmask_data.decode('utf-8'))
-
-@app.route("/application_web/rendu_brut", methods=["GET"])
-def render_raw():
-    img_name = request.args.get("img_name")
-    img = modele.treat_image(img_name)
-
-    im_pil = Image.fromarray(img.astype(np.uint8))
-
-    data = io.BytesIO()
-    im_pil.save(data, "PNG")
-    encoded_img_data = base64.b64encode(data.getvalue())
-    return render_template("rendu.html", img_data=encoded_img_data.decode('utf-8'))
-
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000, host='0.0.0.0')
